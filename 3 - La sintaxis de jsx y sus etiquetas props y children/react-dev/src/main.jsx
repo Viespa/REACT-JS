aeff7f98b11e9a8db1ab32c9c243615fc1dbcf77
Victor Espigares Perez <victorep105ggmail.com>
2023-05-08 22:11:23 +0200

    Create project

---
+++ 3 - La sintaxis de jsx y sus etiquetas props y children/react-dev/src/main.jsx
@@ -0,0 +1,8 @@
+import  ReactDOM from "react-dom";
+import "./index.css"
+
+const app = <h1>Desarrollo Viespa</h1>;
+
+const container = document.getElementById("root");
+
+ReactDOM.render(app, container);
