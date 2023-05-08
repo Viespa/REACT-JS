9b5eb1e0d7631163fb3b4e5eda36f7fff36679f3
Victor Espigares Perez <victorep105ggmail.com>
2023-05-08 22:38:50 +0200

    etiqueta

--- 3 - La sintaxis de jsx y sus etiquetas props y children/react-dev/src/main.jsx
+++ 3 - La sintaxis de jsx y sus etiquetas props y children/react-dev/src/main.jsx
@@ -1,7 +1,15 @@
 import  ReactDOM from "react-dom";
 import "./index.css"
 
-const app = <h1>Desarrollo Viespa</h1>;
+const Title = props => {
+    return <h3 className="title">{props.texto}</h3>
+}
+
+const app = (
+    <div>
+        <Title texto="Desarrollo Viespa"></Title>
+    </div>
+);
 
 const container = document.getElementById("root");
 
