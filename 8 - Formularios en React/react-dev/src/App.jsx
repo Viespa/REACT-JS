f4778237569e091e217690be78b87e1e8f8cfb21
Victor Espigares Perez <victorep105ggmail.com>
2023-05-09 19:10:43 +0200

    create project

---
+++ 8 - Formularios en React/react-dev/src/App.jsx
@@ -0,0 +1,30 @@
+import {useState} from 'react';
+
+const getInitialLikes = () =>{
+    console.log('getInitialLikes');
+    return 12;
+}
+
+const Likes = () =>{
+
+    console.log("Render Likes")
+
+    const [likes ,setLikes] = useState(0);
+    
+    return <Button 
+                onClick={() => setLikes(likes + 1)}>
+                {likes} likes
+            </Button>
+}
+
+const Button = ({onClick, children}) => {
+    console.log("Render Button");
+    return <button onClick={onClick}>{children}</button>
+}
+
+const App = () =>{
+    console.log("Render App")
+ return <Likes/>;
+};
+
+export default App;
